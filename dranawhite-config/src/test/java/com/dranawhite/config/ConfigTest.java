bd1791d7193395f863182e9793836e63d5a68d3c
dranawhite <1458755325@qq.com>
2018-04-17 09:50:28 +0800

    完善项目架构

---
+++ dranawhite-config/src/test/java/com/dranawhite/config/ConfigTest.java
@@ -0,0 +1,29 @@
+package com.dranawhite.config;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author liangyq
+ * @version [1.0, 2018/3/30 16:17]
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+		"classpath:applicationContext-disconf.xml"
+})
+public class ConfigTest {
+
+	@Autowired
+	private RabbitMq rabbitMq;
+
+	@Test
+	public void testDisconf() {
+		String user = rabbitMq.getUsername();
+		Assert.assertEquals(user, "admin");
+	}
+
+}
