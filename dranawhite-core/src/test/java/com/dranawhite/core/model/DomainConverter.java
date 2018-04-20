ebbe87cf162f86f48f82f1a30e1c8b363d0ef947
dranawhite <1458755325@qq.com>
2018-04-20 11:39:20 +0800

    orika使用

---
+++ dranawhite-core/src/test/java/com/dranawhite/core/model/DomainConverter.java
@@ -0,0 +1,26 @@
+package com.dranawhite.core.model;
+
+import ma.glasnost.orika.MapperFacade;
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
+
+/**
+ * @author liangyq
+ * @version [1.0, 2018/4/20 11:21]
+ */
+public class DomainConverter {
+
+	public static void main(String[] args) {
+		MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
+		mapperFactory.classMap(UserDO.class, UserVO.class)
+				.byDefault()
+				.register();
+		UserDO user = new UserDO();
+		user.setId(123);
+		user.setName("小明");
+		MapperFacade mapper = mapperFactory.getMapperFacade();
+		UserVO userVO = mapper.map(user, UserVO.class);
+		System.out.println(userVO.toString());
+	}
+
+}
---
+++ dranawhite-core/src/test/java/com/dranawhite/core/model/UserVO.java
@@ -0,0 +1,16 @@
+package com.dranawhite.core.model;
+
+import lombok.Data;
+
+/**
+ * @author liangyq
+ * @version [1.0, 2018/4/20 11:20]
+ */
+@Data
+public class UserVO {
+
+	private int id;
+	private String name;
+	private int age;
+	private String address;
+}
