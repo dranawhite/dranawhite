c2d9c9fe156d9243d33ac775b52c0873e23c4b83
dranawhite <1458755325@qq.com>
2018-04-26 15:25:07 +0800

    Cglib动态代理基类

---
+++ dranawhite-interceptor/src/main/java/com/dranawhite/interceptor/BaseCglibInterceptor.java
@@ -0,0 +1,46 @@
+package com.dranawhite.interceptor;
+
+import com.dranawhite.exception.DranawhiteException;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+
+import java.lang.reflect.Method;
+
+/**
+ * 抽象Cglib拦截器
+ *
+ * @author liangyq
+ * @version [1.0, 2018/4/26 14:55]
+ */
+public class BaseCglibInterceptor implements MethodInterceptor {
+
+	public <T> T getProxy(Class<T> clz) {
+		return (T) Enhancer.create(clz, this);
+	}
+
+	protected <T> T setUp(){
+		return null;
+	}
+
+	protected <T> void setUp(T t){}
+
+	protected <T> T tearDown() {
+		return null;
+	}
+
+	protected <T> void tearDown(T t){}
+
+	@Override
+	public Object intercept(Object obj, Method method, Object[] args, MethodProxy methodProxy) {
+		try {
+			setUp(obj);
+			Object result = methodProxy.invokeSuper(obj, args);
+			tearDown(obj);
+			return result;
+		} catch (Throwable t) {
+			throw new DranawhiteException("动态代理异常！", t);
+		}
+	}
+}
--- dranawhite-interceptor/src/main/java/com/dranawhite/interceptor/InvokeTimeInterceptor.java
+++ dranawhite-interceptor/src/main/java/com/dranawhite/interceptor/InvokeTimeInterceptor.java
@@ -3,7 +3,6 @@ package com.dranawhite.interceptor;
 import com.dranawhite.exception.DranawhiteException;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
@@ -11,31 +10,36 @@ import java.lang.reflect.Method;
 /**
  * 方法调用计时器
  * <pre>
- *     单位：毫秒
+ *     单位：默认毫秒
  * </pre>
  *
  * @author liangyq
  * @version [1.0, 2018/4/26 13:52]
  */
 @Slf4j
-public class InvokeTimeInterceptor implements MethodInterceptor {
+public class InvokeTimeInterceptor extends BaseCglibInterceptor {
 
-	public <T> T getInstance(Class<T> clz) {
-		return (T) Enhancer.create(clz, this);
+	@Override
+	protected Long setUp() {
+		return System.currentTimeMillis();
+	}
+
+	@Override
+	protected Long tearDown() {
+		return System.currentTimeMillis();
 	}
 
 	@Override
 	public Object intercept(Object obj, Method method, Object[] args, MethodProxy methodProxy) {
 		try {
-			long startTime = System.currentTimeMillis();
+			long startTime = setUp();
 			Object result = methodProxy.invokeSuper(obj, args);
-			long endTime = System.currentTimeMillis();
+			long endTime = tearDown();
 			long time = endTime - startTime;
-			log.info("耗时：" + time + " ms");
+			log.info("耗时：" + time);
 			return result;
 		} catch(Throwable t) {
 			throw new DranawhiteException("计时器异常！", t);
 		}
-
 	}
 }
