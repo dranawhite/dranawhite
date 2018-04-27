d09b8db2703ac4c571a05265bdf4f8627caa68db
dranawhite <1458755325@qq.com>
2018-04-27 10:50:35 +0800

    合并JMH和JMeter工程

---
+++ dranawhite-test/src/main/java/com/dranawhite/test/jmeter/dubbo/AbstractDubboPerformSampler.java
@@ -0,0 +1,48 @@
+package com.dranawhite.test.jmeter.dubbo;
+
+import com.dranawhite.api.model.RespEnum;
+import com.dranawhite.api.model.Result;
+import com.dranawhite.common.util.StringUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+
+/**
+ * Dubbo接口压力测试
+ *
+ * @author liangyq
+ * @version [1.0, 2018/4/25 17:06]
+ */
+@Slf4j
+public abstract class AbstractDubboPerformSampler extends AbstractJavaSamplerClient {
+
+	@Override
+	public SampleResult runTest(JavaSamplerContext javaSamplerContext) {
+		SampleResult result = new SampleResult();
+
+		log.info("Dubbo开启性能测试");
+		long startTime = System.currentTimeMillis();
+		Result invokeResult = run(javaSamplerContext);
+		long endTime = System.currentTimeMillis();
+		log.info("Dubbo完成性能测试， 耗时" + (endTime - startTime) + " 毫秒");
+
+		if (invokeResult == null || StringUtil.isNotEqual(invokeResult.getRespCode(), RespEnum.SUCCESS.getCode())) {
+			result.setSuccessful(Boolean.FALSE);
+		} else {
+			result.setSuccessful(Boolean.TRUE);
+		}
+		return result;
+	}
+
+	/**
+	 * 业务运行方法
+	 *
+	 * @param context 取样器上下文
+	 * @param <T>     数据封装
+	 *
+	 * @return Result
+	 */
+	public abstract <T> Result<T> run(JavaSamplerContext context);
+
+}
