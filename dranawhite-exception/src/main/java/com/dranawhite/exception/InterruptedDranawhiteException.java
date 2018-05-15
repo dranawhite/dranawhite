773f18469e8f6003e40a26d73f3b607b0a8d4bea
dranawhite <1458755325@qq.com>
2018-05-15 16:41:49 +0800

    中断异常

---
+++ dranawhite-exception/src/main/java/com/dranawhite/exception/InterruptedDranawhiteException.java
@@ -0,0 +1,16 @@
+package com.dranawhite.exception;
+
+/**
+ * 中断异常
+ *
+ * @author liangyq
+ * @version [1.0, 2018/5/15 16:40]
+ */
+public class InterruptedDranawhiteException extends DranawhiteException {
+
+	private static final long serialVersionUID = 6491298702040045568L;
+
+	public InterruptedDranawhiteException(String message) {
+		super(message);
+	}
+}
