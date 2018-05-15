d24bced60e44bac159ac5a8ab7751987fbd6c6e0
dranawhite <1458755325@qq.com>
2018-05-15 16:34:26 +0800

    添加监控器状态异常

---
+++ dranawhite-exception/src/main/java/com/dranawhite/exception/IllegalMonitorStateDranawhiteException.java
@@ -0,0 +1,14 @@
+package com.dranawhite.exception;
+
+/**
+ * @author liangyq
+ * @version [1.0, 2018/5/15 16:33]
+ */
+public class IllegalMonitorStateDranawhiteException extends DranawhiteException {
+
+	private static final long serialVersionUID = 8999046153796933280L;
+
+	public IllegalMonitorStateDranawhiteException(String message) {
+		super(message);
+	}
+}
