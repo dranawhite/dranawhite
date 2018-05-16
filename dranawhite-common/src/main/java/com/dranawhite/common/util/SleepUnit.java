9b3a838d850d908ba01120274d0c08bcaac430fa
dranawhite <1458755325@qq.com>
2018-05-16 15:45:54 +0800

    SleepUnit工具类

---
+++ dranawhite-common/src/main/java/com/dranawhite/common/util/SleepUnit.java
@@ -0,0 +1,21 @@
+package com.dranawhite.common.util;
+
+import com.dranawhite.exception.DranawhiteException;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author liangyq
+ * @version [1.0, 2018/5/16 15:44]
+ */
+public final class SleepUnit {
+
+	public void seconds(int seconds) {
+		try {
+			TimeUnit.SECONDS.sleep(seconds);
+		} catch (InterruptedException ex) {
+			throw new DranawhiteException(ex);
+		}
+	}
+
+}
