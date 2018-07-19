e505faabf34ac8f7fb06e501351528ae6e7e958d
dranawhite <1458755325@qq.com>
2018-07-19 15:38:08 +0800

    添加ClientUtil工具类

---
+++ dranawhite-common/src/main/java/com/dranawhite/common/util/ClientUtil.java
@@ -0,0 +1,36 @@
+package com.dranawhite.common.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 客户端工具类
+ *
+ * @author liangyq 2018/07/19
+ */
+public class ClientUtil {
+
+	private static final String UNKNOWN = "unknown";
+
+	/**
+	 * 获取客户端真实ip
+	 *
+	 * @param request 请求
+	 *
+	 * @return IP
+	 */
+	public static String getClientIp(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (StringUtils.isBlank(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (StringUtils.isBlank(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (StringUtils.isBlank(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
+	}
+}
