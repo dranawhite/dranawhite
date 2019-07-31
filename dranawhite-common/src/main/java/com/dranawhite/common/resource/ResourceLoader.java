54738b3e7a902bd86c081f592f4d5dc998d553fd
dranawhite <dranawhite@aliyun.com>
2019-07-31 18:20:52 +0800

    ResourceLoader添加获取File的方法

--- dranawhite-common/src/main/java/com/dranawhite/common/resource/ResourceLoader.java
+++ dranawhite-common/src/main/java/com/dranawhite/common/resource/ResourceLoader.java
@@ -7,6 +7,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -33,6 +34,15 @@ public final class ResourceLoader {
 
     public static Resource getResource(String file) {
         return resolver.getResource(file);
+    }
+
+    public static File getClasspathFile(String file) {
+        try {
+            Resource resource = resolver.getResource(file);
+            return resource.getFile();
+        } catch (IOException ioe) {
+            throw new DranaFileException("加载资源文件失败!", ResultCodeEnum.SERVICE_UNAVAILABLE, ioe);
+        }
     }
 
     public static InputStream getInputStream(String file) {
