5f3a1dbc59739c16b3248b953e8c14eda2a3e59d
dranawhite <dranawhite@aliyun.com>
2019-08-07 18:17:09 +0800

    添加分隔符

--- dranawhite-common/src/main/java/com/dranawhite/common/constants/Separator.java
+++ dranawhite-common/src/main/java/com/dranawhite/common/constants/Separator.java
@@ -7,20 +7,23 @@ package com.dranawhite.common.constants;
  */
 public final class Separator {
 
-	public static final String POINT = ".";
-	public static final String COMMA = ",";
-	public static final String SEMICOLON = ";";
-	public static final String SLASHES = "/";
-	public static final String BACKSLASH = "\\";
-	public static final String UNDERLINE = "_";
-	public static final String MIDDLELINE = "-";
-	public static final String POUND = "#";
-	public static final String ASTERISK = "*";
-	public static final String VERTICAL = "|";
+    public static final String POINT = ".";
+    public static final String COMMA = ",";
+    public static final String SEMICOLON = ";";
+    public static final String SLASHES = "/";
+    public static final String BACKSLASH = "\\";
+    public static final String UNDERLINE = "_";
+    public static final String MIDDLELINE = "-";
+    public static final String POUND = "#";
+    public static final String ASTERISK = "*";
+    public static final String VERTICAL = "|";
+    public static final char CH_SPACE = ' ';
 
-	public static final char CH_LEFT_BRACE = '{';
-	public static final char CH_SPACE = ' ';
-	public static final char CH_LEFT_BRACKET = '[';
-	public static final char CH_LEFT_PARENTHESES = '(';
+    public static final char CH_LEFT_BRACE = '{';
+    public static final char CH_LEFT_BRACKET = '[';
+    public static final char CH_LEFT_PARENTHESES = '(';
+    public static final char CH_RIGHT_BRACE = '}';
+    public static final char CH_RIGHT_BRACKET = ']';
+    public static final char CH_RIGHT_PARENTHESES = ')';
 
 }
